d8672984504e0fd7d97a24aac500d8cd91b3565f
Jose Carlos Mejia Huayhua <jmejiahu@emeal.nttdata.com>
2022-03-03 16:40:10 -0500

    catalogo

---
+++ src/main/java/com/bank/RateBitcoin/model/RateBitcoin.java
@@ -0,0 +1,21 @@
+package com.bank.RateBitcoin.model;
+
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document("rate")
+public class RateBitcoin implements Serializable {
+    @Id
+    String id;
+    String type;
+    Float amount_by_unit;
+
+}
